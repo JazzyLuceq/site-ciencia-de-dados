fcb2830b5885f4df4d9276f4e2d064d2c8deaa84
Karl David Lins <162985408+JazzyLuceq@users.noreply.github.com>
2024-11-19 09:10:15 -0300

    mudanças do site 19/11 09:10

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__,
             template_folder ="siteciencia/templates", static_folder = "siteciencia/static")
 
@@ -7,3 +8,10 @@ app = Flask(__name__,
 def index():
     # return "<h1>Bom dia, professor.</h1>"
     return render_template("index.html")
+
+@app.route("/analise/<usuario>")
+def analise(usuario):
+    return render_template("analise.html", usuario=usuario)
+
+if __name__ == "__main__":
+    app.run(debug=True)
